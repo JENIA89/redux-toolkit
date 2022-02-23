3d727c71bb160e9a84697768c55900a3c54e4e1c
Eugene Popolamov <eugene.popolamov@effective-soft.com>
2022-02-23 19:28:51 +0300

    feat: add fetch request with toolkit

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import { useAppDispatch, useAppSelector } from './hooks/redux';
+import { fetchUsers } from './store/reducers/ActionCreators';
 import { userSlice } from './store/reducers/UserSlice';
 
 function App() {
- 
+  const dispatch = useAppDispatch()
+  const {user, isLoading, error} = useAppSelector(state => state.userReducer)
+
+  useEffect(()=>{
+    dispatch(fetchUsers())
+  }, [])
+
   return (
     <div className="App">
-      
+      {isLoading && <h1>LOADING...</h1>}
+      {error && <h1>{error}</h1>}
+      {JSON.stringify(user, null, 2)}
     </div>
   );
 }
--- src/store/reducers/UserSlice.ts
+++ src/store/reducers/UserSlice.ts
@@ -1,5 +1,6 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { IUser } from './../../models/IUser';
+import { fetchUsers } from './ActionCreators';
 
 interface UserState {
   user: IUser[];
@@ -17,15 +18,29 @@ export const userSlice = createSlice({
   name: 'user',
   initialState,
   reducers: {
-    usersFetching(state){
+    // usersFetching(state){
+    //   state.isLoading = true;
+    // },
+    // usersFetchingSuccess(state, action: PayloadAction<IUser[]>){
+    //   state.isLoading = false;
+    //   state.user = action.payload;
+    //   state.error = '';
+    // },
+    // usersFetchingError(state, action: PayloadAction<string>){
+    //   state.isLoading = false;
+    //   state.error = action.payload;
+    // },
+  },
+  extraReducers: {
+    [fetchUsers.pending.type]: (state) => {
       state.isLoading = true;
     },
-    usersFetchingSuccess(state, action: PayloadAction<IUser[]>){
+    [fetchUsers.fulfilled.type]: (state, action: PayloadAction<IUser[]>) => {
       state.isLoading = false;
       state.user = action.payload;
       state.error = '';
     },
-    usersFetchingError(state, action: PayloadAction<string>){
+    [fetchUsers.rejected.type]: (state, action: PayloadAction<string>) => {
       state.isLoading = false;
       state.error = action.payload;
     },
